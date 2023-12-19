faa8f87b4b24ce523d23917417c4a51155b3d606
Aicha-ch <chwikhi.aycha@gmail.com>
2023-12-19 18:08:27 +0100

    Update code

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 '''
-Python script that retrieves information using a REST API
+Python script that returns information using REST API
 '''
 import requests
 import sys
 
 if __name__ == "__main__":
-    employee_id = sys.argv[1]
-    user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    employee_data = requests.get(user_url).json()
+    get_emp_id = sys.argv[1]
+    user_url = (f'https://jsonplaceholder.typicode.com/users/{get_emp_id}')
+    get_emp_data = requests.get(user_url).json()
+    todos_url = (
+        f'https://jsonplaceholder.typicode.com/todos?userId={get_emp_id}')
+    get_emp_tasks = requests.get(todos_url).json()
 
-    todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-    employee_tasks = requests.get(todos_url).json()
-
-    completed_tasks = [task for task in employee_tasks if task.get("completed")]
+    done_tasks = [task for task in get_emp_tasks if task.get("completed")]
 
     print(
-        f"Employee {employee_data['name']} is done with "
-        f"tasks({len(completed_tasks)}/{len(employee_tasks)}):"
+        f"Employee {get_emp_data['name']} is done with "
+        f"tasks({len(done_tasks)}/{len(get_emp_tasks)}):"
     )
-    for task in completed_tasks:
+    for task in done_tasks:
         print("\t", task["title"])

fae5fe528cdaa14a1c9154ee5481414615efb167
Aicha-ch <chwikhi.aycha@gmail.com>
2023-12-19 20:20:44 +0100

    update code

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,34 +1,9 @@
 #!/usr/bin/python3
 '''
-Python script that retrieves information using a REST API
+Python script that returns information using REST API
 '''
-
 import requests
 import sys
-
-
-def get_employee_data(employee_id):
-    # API endpoint URLs
-    user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-
-    # Fetch employee data
-    employee_data = requests.get(user_url).json()
-
-    # Fetch employee tasks
-    employee_tasks = requests.get(todos_url).json()
-
-    return employee_data, employee_tasks
-
-def display_todo_progress(employee_data, employee_tasks):
-    # Filter completed tasks
-    completed_tasks = [task for task in employee_tasks if task.get("completed")]
-
-    # Display TODO list progress
-    print(f"Employee {employee_data['name']} is done with tasks({len(completed_tasks)}/{len(employee_tasks)}):")
-    
-    for task in completed_tasks:
-        print("\t", task["title"])
 
 if __name__ == "__main__":
     # Check if an employee ID is provided as a command-line argument
@@ -36,14 +11,20 @@ if __name__ == "__main__":
         print("Usage: python3 script_name.py <employee_id>")
         sys.exit(1)
 
-    # Get employee ID from command-line argument
-    employee_id = int(sys.argv[1])
+    employee_id = sys.argv[1]
+    user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    employee_data = requests.get(user_url).json()
+    todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+    employee_tasks = requests.get(todos_url).json()
 
-    # Get employee data and tasks
-    employee_data, employee_tasks = get_employee_data(employee_id)
+    done_tasks = [task for task in employee_tasks if task.get("completed")]
 
-    # Display TODO list progress
-    display_todo_progress(employee_data, employee_tasks)
+    print(
+        f"Employee {employee_data['name']} is done with "
+        f"tasks({len(done_tasks)}/{len(employee_tasks)}):"
+    )
+    for task in done_tasks:
+        print("\t", task["title"])
 
 
 

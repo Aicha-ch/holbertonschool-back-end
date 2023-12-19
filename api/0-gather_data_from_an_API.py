2c46c52e3b1a18272a01310a060a2dbffee10aa8
Aicha-ch <chwikhi.aycha@gmail.com>
2023-12-19 20:22:55 +0100

    update code

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,30 +1,34 @@
 #!/usr/bin/python3
-'''
-Python script that returns information using REST API
-'''
+"""Uses a REST API for a given employee ID, returns
+information  about TODO list progress"""
+
 import requests
 import sys
 
 if __name__ == "__main__":
-    # Check if an employee ID is provided as a command-line argument
     if len(sys.argv) != 2:
-        print("Usage: python3 script_name.py <employee_id>")
+        print(f"missing employee id as argument")
         sys.exit(1)
 
-    employee_id = sys.argv[1]
-    user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    employee_data = requests.get(user_url).json()
-    todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-    employee_tasks = requests.get(todos_url).json()
+    URL = "https://jsonplaceholder.typicode.com"
+    EMPLOYEE_ID = sys.argv[1]
 
-    done_tasks = [task for task in employee_tasks if task.get("completed")]
+    EMPLOYEE_TODOS = requests.get(f"{URL}/users/{EMPLOYEE_ID}/todos",
+                                  params={"_expand": "user"})
+    data = EMPLOYEE_TODOS.json()
 
-    print(
-        f"Employee {employee_data['name']} is done with "
-        f"tasks({len(done_tasks)}/{len(employee_tasks)}):"
-    )
-    for task in done_tasks:
-        print("\t", task["title"])
+    EMPLOYEE_NAME = data[0]["user"]["name"]
+    TOTAL_NUMBER_OF_TASKS = len(data)
+    NUMBER_OF_DONE_TASKS = 0
+    TASK_TITLE = []
+    for task in data:
+        if task["completed"]:
+            NUMBER_OF_DONE_TASKS += 1
+            TASK_TITLE.append(task["title"])
+    print(f"Employee {EMPLOYEE_NAME} is done with tasks"
+          f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+    for title in TASK_TITLE:
+        print("\t ", title)
 
 
 
